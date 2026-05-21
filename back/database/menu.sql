e55a33dc353a88c82861fe43ff7c77dd7869468e
Lara Ovejero <lovejero@fi.uba.ar>
2026-05-21 13:04:01 -0300

    cambio el nombre de la tabla menu por carta y agrego el swagger

--- back/database/menu.sql
+++ back/database/menu.sql
@@ -2,48 +2,43 @@ CREATE DATABASE IF NOT EXISTS restaurante_medieval;
 
 USE restaurante_medieval;
 
-CREATE TABLE menu (
-    id_menu INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE carta (
+    id_plato INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre_plato VARCHAR(100) NOT NULL,
     descripcion TEXT,
     precio DECIMAL(10,2),
     categoria VARCHAR(100),
-    stock BOOLEAN DEFAULT TRUE,
-    popular BOOLEAN DEFAULT FALSE
+    stock BOOLEAN DEFAULT TRUE
 );
 
-INSERT INTO menu
-(nombre_plato, descripcion, precio, categoria, popular)
+INSERT INTO carta
+(nombre_plato, descripcion, precio, categoria)
 VALUES
 
 (
 'Hamburguesa del Rey',
 'Hamburguesa saludable con pan integral',
 8500,
-'Plato Principal',
-TRUE
+'Plato Principal'
 ),
 
 (
 'Papas del Castillo',
 'Papas rusticas con especias medievales',
 4200,
-'Entradas',
-FALSE
+'Entradas'
 ),
 
 (
 'Poción Frutal',
 'Jugo natural de frutos rojos',
 3000,
-'Bebidas',
-TRUE
+'Bebidas'
 ),
 
 (
 'Tarta del Guerrero',
 'Tarta integral de manzana',
 3900,
-'Postres',
-FALSE
+'Postres'
 );

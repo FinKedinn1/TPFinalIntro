65f479fac81103d7b26c8b09a488724136202b6d
Guillermo-gtz <gmgutierrez@fi.uba.ar>
2026-05-22 12:45:04 -0300

    un leve cambio al app.py para poder lanzar el servidor correctamente

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/menu')
-def obtener_carta():
+def menu():
     return render_template("Menu.html")
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template("index.html")
 
 
-
 if __name__ ==  '__main__':
     app.run("127.0.0.1", port="5000", debug=True)
+    
     

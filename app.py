37116b954adc282361154dbb063cd8eb4663d69f
Lour Rosa <lrosa@fi.uba.ar>
2026-05-25 18:09:07 -0300

    arreglo del app.py, hacer los registros, eliminacion de templates innecesarios

--- app.py
+++ app.py
@@ -2,25 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/menu')
+@app.route("/menu")
 def menu():
     return render_template("Menu.html")
 
-@app.route('/historia')
-def historia():
-    return render_template("Historia.html")
+@app.route("/registro")
+def registro():
+    return render_template("registro.html")
 
-@app.route('/contacto')
-def contacto():
-    return render_template("Contacto.html")
-
-@app.route('/rankin')
-def rankin():
-    return render_template("Rankin.html")
-
-if __name__ == '__main__':
-    app.run("127.0.0.1", port="5000", debug=True)
+if __name__ == "__main__":
+    app.run("127.0.0.1", port=5000, debug=True)

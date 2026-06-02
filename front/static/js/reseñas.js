ecf3802c9dc8bb841a2d0b03c751fbe9f179a15e
abril martinelli <abrilmartinelli@eclipse-adhara.local>
2026-06-02 18:29:03 -0300

    integracion de reseñas front-back

--- front/static/js/reseñas.js
+++ front/static/js/reseñas.js
@@ -10,3 +10,57 @@ for (let i = 0; i < estrellas.length; i++) {
         }
     });
 }
+
+document.getElementById("form-reseña").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const comentario = document.getElementById("comentario").value;
+
+    const puntaje = document.getElementsByClassName("activa").length;
+
+    const data = {
+        id_reserva: 1,
+        id_plato: 1,
+        comentario: comentario,
+        puntaje_estrellas: puntaje
+    };
+
+    const res = await fetch("http://127.0.0.1:5000/reseñas", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await res.json();
+    alert(result.Mensaje || result.Error);
+
+    document.getElementById("form-reseña").reset();
+
+    cargarReseñas();
+});
+
+
+async function cargarReseñas() {
+    const res = await fetch("http://127.0.0.1:5000/reseñas");
+    const data = await res.json();
+
+    const contenedor = document.getElementById("contenedor-reseñas");
+
+    contenedor.innerHTML = "";
+
+    data.forEach(r => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+            <p><strong>Comentario:</strong> ${r.comentario}</p>
+            <p><strong>⭐</strong> ${r.puntaje_estrellas}</p>
+            <hr>
+        `;
+
+        contenedor.appendChild(div);
+    });
+}
+
+cargarReseñas();

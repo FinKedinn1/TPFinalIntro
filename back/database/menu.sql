e19e24c1d90873b4fceceee8a00bd57c01001150
abril martinelli <abrilmartinelli@eclipse-adhara.local>
2026-06-05 02:05:20 -0300

    agrego ej de platos para probar platos populares y modifico navbar

--- back/database/menu.sql
+++ back/database/menu.sql
@@ -14,31 +14,78 @@ CREATE TABLE carta (
 INSERT INTO carta
 (nombre_plato, descripcion, precio, categoria)
 VALUES
-
 (
 'Hamburguesa del Rey',
 'Hamburguesa saludable con pan integral',
 8500,
 'Plato Principal'
 ),
-
 (
 'Papas del Castillo',
 'Papas rusticas con especias medievales',
 4200,
 'Entradas'
 ),
-
 (
 'Poción Frutal',
 'Jugo natural de frutos rojos',
 3000,
 'Bebidas'
 ),
-
 (
 'Tarta del Guerrero',
 'Tarta integral de manzana',
 3900,
+'Postres'
+),
+(
+'Costillas del Dragón',
+'Costillas glaseadas con salsa de miel y especias',
+12500,
+'Plato Principal'
+),
+(
+'Sopa del Hechicero',
+'Sopa cremosa de calabaza y hierbas',
+4800,
+'Entradas'
+),
+(
+'Elixir de los Bosques',
+'Limonada con menta y frutos cítricos',
+3200,
+'Bebidas'
+),
+(
+'Banquete de la Reina',
+'Cheesecake de frutos rojos',
+4500,
+'Postres'
+);
+(
+'Pollo del Caballero',
+'Pechuga grillada con hierbas y vegetales',
+9800,
+'Plato Principal'
+),
+
+(
+'Anillos del Reino',
+'Aros de cebolla crocantes con salsa especial',
+4500,
+'Entradas'
+),
+
+(
+'Hidromiel Real',
+'Bebida refrescante inspirada en la hidromiel medieval',
+3500,
+'Bebidas'
+),
+
+(
+'Delicia de la Corona',
+'Brownie tibio con salsa de chocolate',
+4800,
 'Postres'
 );
